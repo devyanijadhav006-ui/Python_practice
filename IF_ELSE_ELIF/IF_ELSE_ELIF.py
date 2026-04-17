bb1462d0eac2c6b33b7c5e66124751a24056c8f8
Devyani Jadhav <devyani.jadhav006@gmail.com>
2026-04-17 20:28:51 +0530

    Comitting new file of IF else

---
+++ IF_ELSE_ELIF/IF_ELSE_ELIF.py
@@ -0,0 +1,64 @@
+# #Solution1
+# Num= (int(input("Enter a num: ")))
+# if Num>0:
+#     print("Number is positive")
+# elif Num<0:
+#     print("Number is negative")
+# else:
+#     print("Number is Zero")
+
+# Solution2
+# Num= int(input("Enter a number: "))
+# if (Num%2==0):
+#     print("Number is Even")
+# else:
+#     print("Number is Odd")
+
+#Solution 3
+# Age =int (input("Enter your Age :"))
+# if Age >= 18:
+#     print("Bro! you are an Adult")
+# else:
+#     print("You are just a kid")
+
+#Solution 4
+# Number = int (input("Enter your number :"))
+# if Number%5==0:
+#     print("Number is divisible by 5")
+# else:
+#     print("Number is not divisible by 5")
+
+#Solution 5
+# Marks= int (input("Enter your number :"))
+# if Marks >=90:
+#     print("Passed with garde A")
+# elif Marks >=75:
+#     print("B")
+# elif Marks >=50:
+#     print("C")
+# else:
+#     print("FAIL")
+
+# #Solution 6
+# Number1 = int (input("Enter your number :"))
+# Number2 = int (input("Enter your number :"))
+# Number3 = int (input("Enter your number :"))
+
+# if Number1>=Number2 and Number1>=Number3:
+#     print("The largest number is ", Number1)
+# elif Number2>=Number1 and Number2>=Number3:
+#     print("The largest number is", Number2)
+# else:
+#     print("The largest number is ", Number3)
+
+# Solution 7
+Year= int(input("Enter a year"))
+if Year % 400 ==0:
+    print("A leap year")
+elif Year % 100 ==0:
+    print("Not a leap year")
+elif Year % 4 ==0:
+ print("A leap year")
+else:
+   print("Not a leap year")
+
