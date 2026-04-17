336720ec507f04e1c4e2cfc3c92c6bc51bc8d280
Devyani Jadhav <devyani.jadhav006@gmail.com>
2026-04-17 19:36:33 +0530

    Pushing Var and DT execries

---
+++ Variables_and_DataTypes/Variable_and_DT_Exercises.py
@@ -0,0 +1,30 @@
+#Exercise 1
+Name = "Devyani"
+Age = 28
+City = "Pune"
+print("My name is", Name, "I am", Age, "years old and I live in", City)
+
+#Exercise 2
+a = 10
+b = 10.5
+c = "Hello"
+d = True
+print(type(a))
+print(type(b))
+print(type(d))
+print(type(a))
+
+#Exercise 3
+x = "50"
+y = 10
+x = (int("50"))
+print(x+y)
+
+#Excersie 4
+a= str(input("Enter your name:"))
+Age =int(input("Enter your Age: "))
+Salary =float(input("Enter your salary: "))
+print("my name is", a,"My age is ", Age, "I earn ",Salary)
+
+
+
